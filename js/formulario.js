415618d74b810ae85ec1cd5e9fb2a7677ae94b2f
stephaniewv <stephaniewv@gmail.com>
2018-01-02 13:31:47 -0500

    Upload final

--- js/formulario.js
+++ js/formulario.js
@@ -1,11 +1,2 @@
-$(document).ready(function () {
-	$('#registrationForm').bind('submit', function(event) {
-		if (form.checkValidity() === false) {
-			event.preventDefault();
-			event.stopPropagation();
-		}
-		form.classList.add('was-validated');
-	}, false);
-});
 
 
--- js/signup.js
+++ js/signup.js
@@ -1,40 +1,57 @@
-$(document).ready(function() {
-  $(function() {
-    $(".dropdown-menu li a").click(function() {
-      console.log($(this));
-      console.log($("select-country"));
-      var img = 
-      $(".btn:first-child").appendChild($(this).innerHTML);
-      $(".btn:first-child").val($(this).text());
+$(document).ready(function () {
+  $(document).ready(function () {
+    $(function () {
+      $(".dropdown-menu li a").click(function () {
+        let cloneImg = $(this).children().clone(false);
+        refreshComboBox($(".btn"), $(this).data("value"));
+        $(".btn").prepend(cloneImg);
+      });
     });
+
+    function refreshComboBox(elem, selectedValue) {
+      // remove TEXT NODE
+      $(elem)
+        .contents()
+        .filter(function () {
+          return this.nodeType == 3;
+        }).remove();
+
+      $(elem).find(":first-child").remove();
+
+      $(elem).val(selectedValue);
+    }
+
   });
+
+  // next
+
+  $('#next').attr('disabled', true);
+  $('#number').keyup(function () {
+    if (($(this).val().length) === 15) {
+      $('#next').attr('disabled', false);
+    }
+    else {
+      $('#next').attr('disabled', true);
+    }
+  });
+
+  // generar codigo
+
+  $('#next').click(function (e) {
+    var code = Math.floor(1e2 + (Math.random() * 2e2));
+    alert('Tu código ' + ' LAB ' + code);
+
+    localStorage.code = code;
+
+    localStorage.getItem(code);
+    window.location.href = 'verify.html'
+
+  });
+
+
+
+
+
 });
-
-// next
-
-$('#next').attr('disabled', true);
-$('#number').keyup(function () {
-  if (($(this).val().length) === 15) {
-    $('#next').attr('disabled', false);
-  }
-  else {
-    $('#next').attr('disabled', true);
-  }
-});
-
-// generar codigo
-
-$('#next').click(function(e) {
-  var code = Math.floor(1e2 + (Math.random() * 2e2));
-  alert('Tu código ' + ' LAB ' + code);
-
-  localStorage.code = code;
-
-  localStorage.getItem(code);
-  window.location.href = 'verify.html'
-
-});
-
-
 
 
